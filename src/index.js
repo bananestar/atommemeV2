da066f7dc610cacc4cc424090af05c0120f7629e
Denisi64 <simonpro.denis@gmail.com>
2024-03-03 16:38:01 +0100

    commit

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Navbar /> // Et ici aussi
-        <BDD />
+        <App />
     </React.StrictMode>
 );
 
